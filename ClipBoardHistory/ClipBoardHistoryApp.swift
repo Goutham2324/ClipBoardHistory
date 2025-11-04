ad0346d0c68660dc1f1f1babdaa7345b39c66bc3
Swejan Kothamasu <gouthamswejan@gmail.com>
2025-11-04 15:03:22 +0530

    Initial Commit

---
+++ ClipBoardHistory/ClipBoardHistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipBoardHistoryApp.swift
+//  ClipBoardHistory
+//
+//  Created by Swejan Kothamasu on 04/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ClipBoardHistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
